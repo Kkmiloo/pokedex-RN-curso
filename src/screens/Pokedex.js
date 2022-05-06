da2d710bfbe9a3beebfd09665e5532397263bfa7
Camilo Reyes <Camilo.reyes_granada@uao.edu.co>
2022-05-05 23:04:00 -0500

    API fetch Pokedex.js

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context';
-
+import { View, Text } from "react-native";
+import { getPokemonsAPI } from "../api/pokemon";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Pokedex() {
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })();
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsAPI();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
     </SafeAreaView>
-  )
+  );
 }
