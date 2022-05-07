2b7ac9dc0c6f48375a1c0a0b15541e24654e1145
Camilo Reyes <Camilo.reyes_granada@uao.edu.co>
2022-05-06 21:31:34 -0500

    Se añadio useState para manejar los estados y se hizo una peticion fetch para tener mas informacion de los pokemons

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -15,3 +15,15 @@ export async function getPokemonsAPI(){
     }
 
 }
+
+export async function getPokemonDetailsByUrlApi(url){
+
+    try {
+        const response = await fetch(url);
+        const result = await response.json();
+        return result
+    } catch (error) {
+        throw error
+    }
+
+}
--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,9 +1,15 @@
 import { View, Text } from "react-native";
-import { getPokemonsAPI } from "../api/pokemon";
+import { getPokemonsAPI, getPokemonDetailsByUrlApi } from "../api/pokemon";
+
 import React, { useState, useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Pokedex() {
+
+  const [pokemons,setPokemons] = useState([]);
+
+  console.log(pokemons);
+
   useEffect(() => {
     (async () => {
       await loadPokemons();
@@ -13,7 +19,28 @@ export default function Pokedex() {
   const loadPokemons = async () => {
     try {
       const response = await getPokemonsAPI();
-      console.log(response);
+      
+      const pokemonsArray =[];
+
+      for await (const pokemon of response.results){
+
+        const pokemonDetail = await getPokemonDetailsByUrlApi(pokemon.url);
+
+        pokemonsArray.push(
+          {
+            id: pokemonDetail.id,
+            name : pokemonDetail.name,
+            type: pokemonDetail.types[0].type.name,
+            order: pokemonDetail.order,
+            image: pokemonDetail.sprites.other['official-artwork'].front_default,
+          }
+        )
+      }
+
+ 
+      setPokemons([...pokemons, ...pokemonsArray])
+   
+
     } catch (error) {
       console.error(error);
     }
